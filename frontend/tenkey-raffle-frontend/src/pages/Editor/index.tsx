5c2458ae123c4785a959bf3d1c4da74faa9b36b9
ai03 <admin@ai03.com>
2025-11-07 12:48:09 +0900

    Add notices regarding data mismatch in editor

--- frontend/tenkey-raffle-frontend/src/pages/Editor/index.tsx
+++ frontend/tenkey-raffle-frontend/src/pages/Editor/index.tsx
@@ -13,6 +13,7 @@ import { PrizesView } from './Views/PrizesView';
 import { CancelsView } from './Views/CancelsView';
 import { useLocation, Switch, Route } from 'wouter';
 import { motion } from 'motion/react';
+import { notifications } from '@mantine/notifications';
 
 export function Editor() {
 
@@ -50,12 +51,61 @@ export function Editor() {
     }
   )
 
-  useEffect(() => {
-    console.log("Mappings changed in parent")
-  }, [getMappingsQuery.data])
-
   const anyLoading = getParticipantsQuery.isLoading || getPrizesQuery.isLoading || getMappingsQuery.isLoading || getCancelsQuery.isLoading
   const anyError = getParticipantsQuery.isError || getPrizesQuery.isError || getMappingsQuery.isError || getCancelsQuery.isError
+
+
+  // 取得データにミスマッチがある場合は警告する
+  useEffect(() => {
+    if (anyLoading) { return }
+
+    let unknownMappingWinnerIds: string[] = []
+    let unknownMappingPrizeIds: string[] = []
+    let unknownCancelReferences: string[] = []
+
+    for (const mapping of getMappingsQuery.data) {
+      if (!getPrizesQuery.data.find(entry => entry.id === mapping.prizeId)) {
+        unknownMappingPrizeIds.push(mapping.prizeId)
+      }
+      if (mapping.winnerId && !getParticipantsQuery.data.find(entry => entry.registrationId === mapping.winnerId)) {
+        unknownMappingWinnerIds.push(mapping.winnerId)
+      }
+    }
+
+    for (const cancelId of getCancelsQuery.data) {
+      if (!getParticipantsQuery.data.find(entry => entry.registrationId === cancelId)) {
+        unknownCancelReferences.push(cancelId)
+      }
+    }
+
+    if (unknownMappingWinnerIds.length > 0) {
+      notifications.show({
+        color: "red",
+        title: "エラー",
+        message: `以下の抽選結果マッピングに利用されている当選者IDに対応する参加者データが存在しません：${unknownMappingWinnerIds.join('、')}`,
+        autoClose: 7000,
+      })
+    }
+
+    if (unknownMappingPrizeIds.length > 0) {
+      notifications.show({
+        color: "red",
+        title: "エラー",
+        message: `以下の抽選結果マッピングに利用されている景品IDに対応する景品データが存在しません：${unknownMappingPrizeIds.join('、')}`,
+        autoClose: 7000,
+      })
+    }
+
+    if (unknownCancelReferences.length > 0) {
+      notifications.show({
+        color: "red",
+        title: "エラー",
+        message: `以下の当日不参加リストに含まれている受付番号が参加者一覧に存在しません：${unknownCancelReferences.join('、')}`,
+        autoClose: 7000,
+      })
+    }
+
+  }, [anyLoading, getParticipantsQuery.data, getPrizesQuery.data, getMappingsQuery.data, getCancelsQuery.data])
 
   return (
     <AppShell
